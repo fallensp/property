b5f0f4cc4d6d0ddabbc30ce048eaf10685ddf600
Ivan <ivan@Ivans-MacBook-Air.local>
2025-11-10 10:08:22 +0800

    fix create listing bug

--- lib/api/metadata.ts
+++ lib/api/metadata.ts
@@ -1,6 +1,24 @@
 import { apiFetch } from "./client";
 import type { ApiPropertyType } from "./types";
 
+type PropertyTypeResponse =
+  | ApiPropertyType[]
+  | {
+      data?: ApiPropertyType[];
+    };
+
 export async function fetchPropertyTypes(): Promise<ApiPropertyType[]> {
-  return apiFetch<ApiPropertyType[]>("/metadata/property-types");
+  const response = await apiFetch<PropertyTypeResponse>(
+    "/metadata/property-types",
+  );
+
+  if (Array.isArray(response)) {
+    return response;
+  }
+
+  if (response?.data && Array.isArray(response.data)) {
+    return response.data;
+  }
+
+  throw new Error("Invalid property types response");
 }
